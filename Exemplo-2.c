b200ee7fcbf35cfbf410a9dac23331b6c8460d44
Beni_BGW <118313691+benibgw@users.noreply.github.com>
2025-04-14 21:04:35 -0300

    Update Exemplo-2.c

--- Exemplo-2.c
+++ Exemplo-2.c
@@ -3,29 +3,17 @@
 
 main() {
 
-    int a, b; // novamente cria as variaveis que o usuario ir· definir o valor
+    int a, b; // novamente cria as variaveis que o usuario ir√° definir o valor
 
     printf("\n Entre com dois numeros : ");
-    scanf("%d %d", &a, &b); // "printf" e "scanf" acho que j· est· +- explicado
+    scanf("%d %d", &a, &b); // "printf" e "scanf" acho que j√° est√° +- explicado
 
     float potenciacao = pow(a , b);
     float raizQuadrada = sqrt(a);
     float porcentagem = (a / 100) * b;
     float raizQualquerIndice = pow(a, 1 / b); // cria 4 novas variaveis, que operam os valores definidos pelo usuario
 
-    printf("\n Os resultados das operacoes ficaram : Potenciacao %.2f, Raiz-quadrada %.2f, Porcentagem %.2f e Raiz com outro indice %.2f", potenciacao, raizQuadrada, porcentagem, raizQualquerIndice); // resultado das operaÁıes
+    printf("\n Os resultados das operacoes ficaram : Potenciacao %.2f, Raiz-quadrada %.2f, Porcentagem %.2f e Raiz com outro indice %.2f", potenciacao, raizQuadrada, porcentagem, raizQualquerIndice); // resultado das opera√ß√µes
 
-return 0;
+return 0; // retorna o valor da fun√ß√£o
 }
-
- /* nesse exemplo mostro os comandos da biblioteca "math"
-    ,tmb acho bom dar uma olhada sobre bibliotecas
-    ,e enquanto fiz esse exemplo lembrei dos tipos de variaveis
-    acho bom olhar sobre tambem.
-
-    no mais È isso, mesma coisa do ultimo exemplo
-    , anota o que acha importante e n„o lembra,
-    tenta fazer algum programa com essas funÁıes tmb
-    e qualquer duvida pergunta.
-
-    Boa sorte pq vai precisar :) */
